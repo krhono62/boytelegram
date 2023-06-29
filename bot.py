f05652236f12804591568677626ea7f8013c8203
Miguel Barcenas <miguelbarcenasp62@gmail.com>
2023-06-28 18:21:37 -0600

    fixes

--- bot.py
+++ bot.py
@@ -17,7 +17,6 @@ def registro_handler():
         response = requests.get(url, verify=False, timeout=7)
         print(response.text)
         if '"evioCodigo":"NOK"}' in response.text:
-            print("asdhjashjdgghjasdgjhasd")
             return "💚 SIN REGISTRO MI TELMEX 💚"
         else:
             
@@ -49,6 +48,41 @@ def consultarExtras(telefono):
 def consultar_extras_handler():
     telefono = request.args.get('telefono')
     result = consultarExtras(telefono)
+    return result
+
+def direccion(resultado):
+    url = "https://gfcomercial.telmex.com/iupsell/condomicilio"
+
+    headers = {
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'es-419,es;q=0.6',
+        'Connection': 'keep-alive',
+        'Content-Length': '57',
+        'Content-Type': 'application/json',
+        'Host': 'gfcomercial.telmex.com',
+        'Origin': 'https://mitelmex.telmex.com',
+        'Referer': 'https://mitelmex.telmex.com/',
+        'sec-ch-ua': '"Chromium";v="112", "Brave";v="112", "Not:A-Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-site',
+        'Sec-GPC': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
+    }
+    print(resultado)
+    data = {"strvar": str(resultado)}
+
+    response = requests.post(url, headers=headers, json=data)
+    print(response)
+    return response.text
+
+@app.route('/direccion', methods=['POST'])
+def direcciones():
+    telefono = request.form.get('resultado')
+    result = direccion(telefono)
     return result
 
 if __name__ == '__main__':
